ca3f0ca812e85fc0c0b472a539f7fb1b2d62b3f1
odin5on <108775964+odin5on@users.noreply.github.com>
2023-04-22 19:20:49 -0500

    Implement basic processing features for shower data

--- form.js
+++ form.js
@@ -12,6 +12,8 @@ var firebaseConfig = {
   measurementId: "G-KMD7EQTCC2",
 };
 
+var userData;
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -57,11 +59,53 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-function routetoHome(){
-  if(window.location.href.includes("index.html")){
-    
+function routetoHome() {
+  if (window.location.href.includes("index.html")) {
+
   }
   else {
     window.location.href = "./index.html";
   }
+}
+
+function displayName(uid) {
+
+  console.log(uid);
+  firebase.database().ref(`users/${uid}/`).on('value', (snapshot) => {
+    console.log(snapshot.val());
+  });
+  document.getElementsByClassName('body')
+
+}
+
+function loadUserData(uid) {
+  console.log(uid);
+  firebase.database().ref(`users/${uid}/`).on('value', (snapshot) => {
+    userData = snapshot.val();
+    console.log(userData);
+    document.getElementById('welcome').innerHTML = 'Welcome ' + userData.name;
+    processShowerData(userData.showers, userData.gpm);
+  });
+}
+
+function processShowerData(showersData, gpm) {
+  var showers = []
+  Object.keys(showersData).forEach(key => {
+    var shower = { start: '', end: '' };
+    shower.start = new Date(userData.showers[key].start);
+    shower.end = new Date(userData.showers[key].end);
+    showers.push(shower);
+  });
+  var totalMinutes = 0;
+  showers.forEach(shower => {
+    const sh = document.createElement("p");
+    minutes = (Math.round((shower.end - shower.start) / 600) / 100);
+    totalMinutes += minutes;
+    sh.innerHTML = shower.start.toLocaleString('en-US') + " - " + shower.end.toLocaleString() + " Minutes: " + minutes;
+    sh.classList.add("shower");
+    document.getElementById('showers').appendChild(sh);
+  });
+  const totalMin = document.createElement('p');
+  totalMin.innerHTML = 'Total minutes: ' + Math.round(totalMinutes * 100) / 100 + '<br>Total gallons: ' + Math.round(totalMinutes * gpm * 100) / 100;
+  document.getElementById('totals').appendChild(totalMin);
 }
